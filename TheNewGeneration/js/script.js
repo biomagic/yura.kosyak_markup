7e53d7d39f80d367505c6f00b3314681102f823a
biomagic <2755527@mail.ru>
2016-12-17 21:53:44 +0200

    All makets corrections

--- TheNewGeneration/js/script.js
+++ TheNewGeneration/js/script.js
@@ -1,16 +1,15 @@
 $(document).ready(function() {
 
-	$(".categories__li:first").toggleClass("open").find(".submenu").css("display", "block");
+	$(".categories__li:first").toggleClass("open");
 
 	$(".categories__li").on('click', function(event) {
 		event.preventDefault();
-
-		if ($(this).find(".submenu").css("display") == "none") {
-			$(".categories__li").removeClass("open").find(".submenu").css("display", "none");
-			$(this).toggleClass("open").find(".submenu").css("display", "block");
+		if ($(this).find(".submenu").css("visibility") == "hidden") {
+			$(".categories__li").removeClass("open").find(".submenu").css("visibility", "hidden");
+			$(this).toggleClass("open").find(".submenu").css("visibility", "visible");
 		} else {
 			$(".categories__li").removeClass("open");
-			$(this).find(".submenu").css("display", "none");
+			$(this).find(".submenu").css("visibility", "hidden");
 		}		
 	});
 })
